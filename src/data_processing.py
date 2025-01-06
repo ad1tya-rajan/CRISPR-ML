f76f48c46607db3964925d46bd3ca133f36fa9f2
adityarajan <adityarajan53@gmail.com>
2025-01-07 00:46:25 +0530

    data processing implemented

---
+++ src/data_processing.py
@@ -0,0 +1,41 @@
+# Data Processing
+
+import os
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+input_path = "data/raw/guideseq.csv"
+output_path = "data/processed/processed_guideseq.csv"
+
+def process_data(input_path, output_path):
+    print("Loading data...")
+
+    try:
+        data = pd.read_csv(input_path)
+        print(f"Dataset loaded successfully, with dim. {data.shape}")
+    except Exception as e:
+        print(f"Error loading dataset: {e}")
+        return
+
+    print("Cleaning data...")
+    data = data.drop_duplicates()
+    data = data.dropna()
+
+    valid_bool_mask = data['On'].str.match(r'^[ACGTN]+$') & data['Off'].str.match(r'^[ACGTN]+$')        # check regex to validate nucleotide sequence
+    data = data[valid_bool_mask]
+    print("Data cleaned.")
+
+    if 'Reads' in data.columns:
+        print("Normalising 'Reads' column")
+        scaler = MinMaxScaler()
+        data['Reads'] = scaler.fit_transorm(data['Reads'])
+
+    if 'Active' in data.columns:
+        print("One-hot encoding 'Active' column")
+        data['Active'] = data['Active'].astype(int).clip(0, 1)
+
+    try:
+        data.to_csv(output_path, index = False)
+        print(f"Processed data saved to {output_path}")
+    except Exception as e:
+        print(f"Error saving processed data: {e}")
